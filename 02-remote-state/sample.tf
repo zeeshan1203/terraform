9353d993116cd64a8f0492fd99cb1919aafcf28f
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-08-29 19:49:27 +0530

    added 02-remote-state sample.tf

---
+++ 02-remote-state/sample.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "sample" {
+  ami                     = "ami-074df373d6bafa625"
+  instance_type           = "t2.micro"
+}
+
+terraform {
+  backend "s3" {
+    bucket                = "terraform-szs"
+    key                   = "sample/terraform.tfstate"
+    region                = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
