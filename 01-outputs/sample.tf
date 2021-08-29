68c6f0b977fef9ef815217bf44f466783328fdb4
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-08-29 19:01:16 +0530

    updated 01-outputs sample.tf

--- 01-outputs/sample.tf
+++ 01-outputs/sample.tf
@@ -1,7 +1,16 @@
 output "sample" {
-  value = "Hello World"
+  //value = "Hello World"
+  value = var.sample
 }
 
 output "sample1" {
-  value = "Hello World1"
+  value = "${var.sample}"
 }
+
+variable "sample" {
+  default = "Hello World"
+}
+
+## Variable sample can be accessed as var.sample or ${var.sample}, However
+## var.sample is used without quotes
+## ${var.sample} is used within quotes.
