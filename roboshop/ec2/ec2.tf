aac447870244d2ccb456f86a160b20aa9a3f4a77
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-08-30 11:53:20 +0530

    updated roboshop ec2 ec2.tf

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -1,9 +1,12 @@
 resource "aws_spot_instance_request" "cheap_worker" {
+  count               = length(var.COMPONENTS)
   ami                 = "ami-074df373d6bafa625"
-  spot_price          = "0.0035"                 ##copy the ur spot request price from aws
+  spot_price          = "0.0035"
   instance_type       = "t2.micro"
 
   tags = {
-    Name              = "sample"
+    Name              = element(var.COMPONENTS, count.index)
   }
 }
+
+variable "COMPONENTS" {}
