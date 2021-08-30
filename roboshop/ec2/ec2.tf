53bcf995b1072394b914bc0ff3185733ba0de0e4
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-08-30 12:05:36 +0530

    updated roboshop ec2, ec2.tf

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -22,3 +22,12 @@ resource "aws_ec2_tag" "spot" {
   key                 = "Name"
   value               = element(var.COMPONENTS, count.index)
 }
+
+resource "aws_route53_record" "dns" {
+  count               = length(var.COMPONENTS)
+  zone_id             = " Z0455275BOCGUWER4BVO"  ##ur route 53 dns zone id
+  name                = "${element(var.COMPONENTS, count.index)}.roboshop.internal"
+  type                = "A"
+  ttl                 = "300"
+  records             = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
+}
