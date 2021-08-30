22bb4a2c233f12abe88df9e363364500eeae5055
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-08-30 12:36:00 +0530

    updated and modified roboshop ansible-apply ansible.tf

--- roboshop/ansible-apply/ansible.tf
+++ roboshop/ansible-apply/ansible.tf
@@ -1,4 +1,7 @@
 resource "null_resource" "ansible-apply" {
+  triggers = {
+    abc = timestamp()
+  }
   count               = length(var.COMPONENTS)
   provisioner "remote-exec" {
 
@@ -12,7 +15,7 @@ resource "null_resource" "ansible-apply" {
       "sudo yum install ansible -y",
       "sudo yum remove ansible -y",
       "sudo pip install ansible==3.4.0",
-      "ansible-pull -i localhost, -U https://github.com/zeeshan1203/ansible.git  roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
+      "ansible-pull -i localhost, -U https://github.com/zeeshan1203/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
     ]
 
   }
