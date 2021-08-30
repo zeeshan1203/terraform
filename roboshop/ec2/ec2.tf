699532a0c040660de404cfb1cddbca541d158822
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-08-30 11:56:58 +0530

    updated roboshop ec2, ec2.tf

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -4,9 +4,14 @@ resource "aws_spot_instance_request" "cheap_worker" {
   spot_price          = "0.0035"
   instance_type       = "t2.micro"
 
-  tags = {
+  tags                = {
     Name              = element(var.COMPONENTS, count.index)
   }
 }
 
 variable "COMPONENTS" {}
+
+resource "time_sleep" "wait" {
+  depends_on          = [aws_spot_instance_request.cheap_worker]
+  create_duration     = "120s"
+}
