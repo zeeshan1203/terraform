9a0a67e27384357703b54fe4751caca24a237762
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-08-30 12:37:24 +0530

    updated and modified roboshop ansible-apply ansible.tf

--- roboshop/ansible-apply/ansible.tf
+++ roboshop/ansible-apply/ansible.tf
@@ -14,7 +14,8 @@ resource "null_resource" "ansible-apply" {
     inline = [
       "sudo yum install ansible -y",
       "sudo yum remove ansible -y",
-      "sudo pip install ansible==3.4.0",
+      "sudo rm -rf /usr/lib/python2.7/site-packages/ansible*",
+      "sudo pip install ansible",
       "ansible-pull -i localhost, -U https://github.com/zeeshan1203/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
     ]
 
