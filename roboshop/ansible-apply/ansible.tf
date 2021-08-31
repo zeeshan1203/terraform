db8dcd54185c18bfbca1466c72ea0b4cb21334e2
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-08-31 12:01:19 +0530

    modified roboshop ansible-apply ansible.tf

--- roboshop/ansible-apply/ansible.tf
+++ roboshop/ansible-apply/ansible.tf
@@ -12,7 +12,10 @@ resource "null_resource" "ansible-apply" {
     }
 
     inline = [
-      "sudo labauto",
+      "sudo yum install ansible -y",
+      "sudo yum remove ansible -y",
+      "sudo rm -rf /usr/lib/python2.7/site-packages/ansible*",
+      "sudo pip install ansible",
       "ansible-pull -i localhost, -U https://github.com/zeeshan1203/ansible.git roboshop-pull.yml -e COMPONENT=${element(var.COMPONENTS, count.index)}"
     ]
 
