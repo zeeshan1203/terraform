074b48cc2e835490eb510c9775eab1390d9e83d4
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-09-29 14:22:23 +0530

    updated and modified

--- roboshop/vars.tf
+++ roboshop/vars.tf
@@ -1,3 +1,7 @@
+//variable "COMPONENTS" {
+//  default = ["frontend", "mongodb", "catalogue", "redis", "user", "cart", "mysql", "shipping", "rabbitmq", "payment"]
+//}
+
 variable "COMPONENTS" {
-  default = ["frontend", "mongodb", "catalogue", "redis", "user", "cart", "mysql", "shipping", "rabbitmq", "payment"]
+  default = ["cart"]
 }
